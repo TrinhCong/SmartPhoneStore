6e3c25db8d1ae0ef2581b50cca832046b9030d98
vmdung99 <38523256+vmdung99@users.noreply.github.com>
2018-12-02 12:07:24 +0700

    Add files via upload

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,93 +13,120 @@ import java.sql.Statement;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.DBConnection;
 
 /**
  *
  * @author tvcpr
  */
-public class HomeController extends BaseController {
-    
-    
+public class HomeController {
+
+    private Connection connection;
+
     public HomeController(Connection connect) {
-        super(connect);
+        connection = connect;
     }
-    
+
     Scanner sc = new Scanner(System.in);
-    public void ShowMenu(){
-        System.out.println("-------------DashBoard--------------");
+
+    public void ShowMenu() {
+        System.out.println("-------------Cửa sổ trang chủ--------------");
         int choice;
-        Statement stm ;
-        ResultSet rs;
+        Statement stm = null;
+        ResultSet rs = null;
+        Boolean isValid = false;
         try {
             stm = connection.createStatement();
             rs = stm.executeQuery("select * from Products");
-            while(rs.next()){
-                System.out.println("id: "+rs.getString(1)+"\tName: " + rs.getString(2) + "\tPrice: "+rs.getString(3));
+            if (rs.next() == false) {
+                System.out.println("Has no product");
+            } else {
+                do {
+                    System.out.println("id: " + rs.getString(1) + "\tName: " + rs.getString(2) + "\tPrice: " + rs.getString(3));
+                    String choice1Str = "";
+                    int choice1;
+                    do {
+                        System.out.println("What do you do next?");
+                        System.out.println("1. See detalis of product");
+                        System.out.println("2. Back to mainpage");
+                        do {
+                            System.out.println("Please enter an option: ");
+                            choice1Str = sc.nextLine();
+                            if (isNumeric(choice1Str)) {
+                                break;
+                            } else {
+                                System.out.println("choice must be a number!");
+                            }
+                        } while (true);
+                        choice1 = (int) Double.parseDouble(choice1Str);
+                        switch (choice1) {
+                            case 1:
+                                ShowDetail();
+                                break;
+                            case 2:
+
+                                break;
+                            default:
+                                System.out.println("the choice is wrong!");
+                                break;
+                        }
+                    } while (choice1 != 2);
+                } while (rs.next());
             }
+
         } catch (SQLException e) {
-            System.out.println("loi gi do"+e);
+            System.out.println("has some wrong" + e);
         }
-                
-        String choice1Str="";
-        int choice1;
-        do {
-            System.out.println("What do you do next?");
-            System.out.println("1. See detalis of product");
-            System.out.println("2. Back to mainpage");
-            do {
-                System.out.println("Please enter an option: ");
-                choice1Str = sc.nextLine();
-                if (isNumeric(choice1Str)){
-                    break;
-                }else{
-                    System.out.println("choice must be a number!");
-                }
-            } while (true);
-            choice1 = (int) Double.parseDouble(choice1Str);
-            switch (choice1){
-                case 1:
-                    ShowDetail();
-                    break;
-                case 2:
-                    
-                    break;
-                default:
-                    System.out.println("the choice is wrong!");
-                    break;
-            }
-        } while (choice1!=2);
-        
-        
+
     }
-    
-    public void ShowDetail(){        
+
+    public void ShowDetail() {
         String idStr = "";
         double id;
         do {
             System.out.println("please enter ID of product you want to see details: ");
             idStr = sc.nextLine();
-            if (isNumeric(idStr)){
+            if (isNumeric(idStr)) {
                 break;
-            }else{
+            } else {
                 System.out.println("ID must be number!");
             }
-        } while (true); 
-        id=Double.parseDouble(idStr);
+        } while (true);
+        id = Double.parseDouble(idStr);
         try {
-            
+
             Statement st = connection.createStatement();
             ResultSet r = st.executeQuery("select * from Products");
-            while(r.next()){
-                if (r.getDouble(3)==id)
-                System.out.println("id: "+r.getString(1)+"\tName: " + r.getString(2) + "\tPrice: "+r.getString(3) +"\t Weight: "+r.getString(7)+"\t Color: "+r.getString(8));
+            while (r.next()) {
+                if (r.getDouble(3) == id) {
+                    System.out.println("id: " + r.getString(1) 
+                            + "\n Name: " + r.getString(2) 
+                            + "\n Price: " + r.getString(3) 
+                            + "\n ImagePath: " + r.getString(4)
+                            + "\n Quantity: " + r.getString(5)
+                            + "\n Dimension: " + r.getString(6)
+                            + "\n Weight: " + r.getString(7)
+                            + "\n Color: " + r.getString(8)
+                            + "\n Warranty: " + r.getString(9)
+                            + "\n SoundType: " + r.getString(10)
+                            + "\n StartPromotion: " + r.getString(11)
+                            + "\n EndPromotion: " + r.getString(12)
+                            + "\n ConnectionType: " + r.getString(13)
+                            + "\n Memory: " + r.getString(14)
+                            + "\n PromotionPrice: " + r.getString(15)
+                            + "\n Battery: " + r.getString(16)
+                            + "\n OS: " + r.getString(17)
+                            + "\n SDCard: " + r.getString(18)
+                            + "\n Camera: " + r.getString(19) 
+                            + "\n CategoryId: " + r.getString(20));
+                }
             }
 
         } catch (SQLException ex) {
             Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public boolean isNumeric(String str) {
         try {
             double d = Double.parseDouble(str);
