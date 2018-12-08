eac4331e7d4beaa9328b8aea63750513715a1985
vmdung99 <38523256+vmdung99@users.noreply.github.com>
2018-12-08 21:39:04 +0700

    Add files via upload

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -8,7 +8,12 @@ package controller;
 
 import enums.EnumPosition;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,14 +30,14 @@ public class HomeController extends BaseController {
 
     Scanner sc = new Scanner(System.in);
 
-
     public void showMenu() {
         makeSpace(EnumPosition.DASH_TOP);
         System.out.println("----- Home -------");
         _productManager.showAll();
         System.out.println("Options:");
         System.out.println("  1.Product details");
-        System.out.println("  2.Back to main menu");
+        System.out.println("  2.Order");
+        System.out.println("  3.Back to main menu");
     }
 
     public void manageMenu() {
@@ -45,6 +50,9 @@ public class HomeController extends BaseController {
                     showDetail();
                     break;
                 case 2:
+                    orderProduct();
+                    break;
+                case 3:
                     break;
                 default:
                     System.out.println("Option is invalid!");
@@ -58,4 +66,48 @@ public class HomeController extends BaseController {
         _productManager.showDetailById(Id);
     }
 
+    public void orderProduct() {
+        boolean isStop;
+        int r;
+        int r1;
+        int r2;
+        ResultSet rs1 = null;
+        ResultSet rs2 = null;
+        try {
+            do {
+                isStop = false;
+                Statement st = connection.createStatement();
+                Statement st1 = connection.createStatement();
+                Statement st2 = connection.createStatement();
+                System.out.println("Name: ");
+                String name = sc.nextLine();
+                System.out.println("Phone Number: ");
+                String phonenumber = sc.nextLine();
+                System.out.println("Email: ");
+                String email = sc.nextLine();
+                System.out.println("ID of product you want order: ");
+                int Id = enterNumber("Product ID");
+                r = st.executeUpdate("insert into Customers(Name,PhoneNumber,Email) values('" + name + "','" + phonenumber + "','" + email + "')");
+                System.out.println("Insert Successfull: " + name + "\t" + phonenumber + "\n");
+                ResultSet rs = st.executeQuery("select Id from Customers");
+                while(rs.next()){
+                    String customerid = rs.getString(1);
+                    r1 = st1.executeUpdate("insert into Bills(CustomerId) values("+customerid+")");
+                }
+                rs1 = st.executeQuery("select Id from Bills");
+                while(rs1.next()){
+                    String billid = rs1.getString(1);
+                    r2= st2.executeUpdate("insert into BillsDetail(BillId) values("+billid+")");
+                }
+                System.out.println("Do you want to order more? (Y/N)");
+                String choice = sc.nextLine();
+                if (!choice.equalsIgnoreCase("y")) {
+                    isStop = true;
+                }
+            } while (!isStop);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
